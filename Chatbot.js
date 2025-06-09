6d95389fc036c893aaa01555bacc3ac03c702972
ajjurathod <131947107+ajjurathod@users.noreply.github.com>
2025-06-09 10:43:52 +0530

    Update Chatbot.js

--- Chatbot.js
+++ Chatbot.js
@@ -1,38 +1,21 @@
-module.exports = 
-{
+const natural = require('natural');
+const tokenizer = new natural.WordTokenizer();
 
-	ChatbotReply:function(message)
-	{
-		// Bot's data
-		this.Bot_Age = 22;
-		this.Bot_Name = "Abdelrahman Radwan";
-		this.Bot_University = "Ain Shams University";
-		this.Bot_Country = "Egypt";
-		// User data
-		this.User_Age;
-		this.User_Name;
-		this.User_University;
-		// Message processing... 
-		message= message.toLowerCase()
+function ChatbotReply(message) {
+    const tokens = tokenizer.tokenize(message.toLowerCase());
 
-		if(message.indexOf("hi") > -1 || message.indexOf("hello") > -1 || message.indexOf("welcome") > -1 )
-		{
-			return "Hi!";
-		} 
-		else if(message.indexOf("age") > -1 && message.indexOf("your"))
-		{
-			return "I'm " + this.Bot_Age;
-		}
-		else if (message.indexOf("how") > -1 && message.indexOf("are") && message.indexOf("you"))
-		{
-			return "I'm fine ^_^"
-		}
-		else if(message.indexOf("where") > -1 && message.indexOf("live") && message.indexOf("you"))
-		{
-			return "I live in" + this.Bot_Country;
-		}
-		return "Sorry, I didn't get it :( ";
-	}
+    if (tokens.includes('hi') || tokens.includes('hello') || tokens.includes('hey')) {
+        return "Hi there!";
+    }
+    if (tokens.includes('bye') || tokens.includes('goodbye')) {
+        return "Goodbye! Have a nice day!";
+    }
+    if (tokens.includes('thanks') || tokens.includes('thank')) {
+        return "You're welcome!";
+    }
+    if (tokens.includes('joke')) {
+        return "Why did the computer get cold? Because it forgot to close Windows! 😂";
+    }
 
-
-};
+    return "Sorry, I didn't get it :(";
+}
